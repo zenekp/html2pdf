1c48449d973fcabf02936cbb39660cdacb43dbbf
Zenek Palmer <zen.palmer@gmail.com>
2021-06-17 12:31:47 +0100

    Update locale.class.php

--- _class/locale.class.php
+++ _class/locale.class.php
@@ -65,6 +65,7 @@ class HTML2PDF_locale
         $handle = fopen($file, 'r');
         while (!feof($handle)) {
             $line = fgetcsv($handle);
+            if (!is_array($line)) continue;
             if (count($line)!=2) continue;
             self::$_list[trim($line[0])] = trim($line[1]);
         }
